5ed7647b49c948abbb70afe4a455d4375733b574
joselatines <joselatines33@gmail.com>
2023-11-18 20:14:38 -0400

    feat(context): add saving in localStorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,45 +1,67 @@
 import { createContext, ReactNode, useState } from "react";
 
 interface IUser {
-	id: string;
-	username: string;
-	email: string;
-	role: string;
-	profilePic: string;
-	provider: string | "github" | "google" | "twitter";
+  id: string;
+  username: string;
+  email: string;
+  role: string;
+  token: string;
+  profilePic: string;
+  provider: string | "github" | "google" | "twitter";
 }
 
 interface IAuthContext {
-	user: IUser | null;
-	setUser: (value: IUser | null) => void;
+  user: IUser | null;
+  setUser: (value: IUser | null) => void;
 }
 
 interface AuthProviderProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 const AuthContext = createContext<IAuthContext>({
-	user: null,
-	setUser: () => {},
+  user: null,
+  setUser: () => {},
 });
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-	const fakeUser = {
-		id: "72e19fa5-0b59-46a0-b7a9-03193dab5853",
-		username: "John",
-		email: "johndoe@gmail.com",
-		role: "user",
-
-		profilePic:
-			"https://media.licdn.com/dms/image/D4E03AQGWw4A4tGQE8w/profile-displayphoto-shrink_800_800/0/1676919170139?e=2147483647&v=beta&t=jkJl32RI3U7qFNxUTlKLTWBFdy6DzZz0al8cLt0goEs",
+  const fakeUser = {
+    id: "72e19fa5-0b59-46a0-b7a9-03193dab5853",
+    username: "John",
+    email: "johndoe@gmail.com",
+    role: "user",
+    token: "example token",
+    profilePic:
+      "https://media.licdn.com/dms/image/D4E03AQGWw4A4tGQE8w/profile-displayphoto-shrink_800_800/0/1676919170139?e=2147483647&v=beta&t=jkJl32RI3U7qFNxUTlKLTWBFdy6DzZz0al8cLt0goEs",
+  };
+	const getUserFromLocalStorage = (): IUser | null => {
+		const userString = localStorage.getItem("user");
+		if (userString) {
+			return JSON.parse(userString);
+		}
+		return null;
 	};
-	const [user, setUser] = useState<IUser | null>(null);
+  const [user, setUser] = useState<IUser | null>(getUserFromLocalStorage());
 
-	return (
-		<AuthContext.Provider value={{ user, setUser }}>
-			{children}
-		</AuthContext.Provider>
-	);
+  const saveUserToLocalStorage = (user: IUser) => {
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+
+
+  const handleSetUser = (value: IUser | null) => {
+    if (value) {
+      saveUserToLocalStorage(value);
+    } else {
+      localStorage.removeItem("user");
+    }
+    setUser(value);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, setUser: handleSetUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContext, AuthProvider };
--- src/routes/me.tsx
+++ src/routes/me.tsx
@@ -3,15 +3,19 @@ import { useFetch } from "../hooks/useFetch";
 import { API_URL } from "../CONST";
 
 function MeRoute() {
-	const { data, error } = useFetch<any>(`${API_URL}/users/me`);
+	const { data, error } = useFetch<any>(`${API_URL}/users/me`, {
+		headers: {
+			Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkxM2UwZGEyLWE1NTgtNDhhOS1iMmFkLWExNzQ4NmU3ZjhhYyIsInJvbGUiOiJ1c2VyIiwiZW1haWwiOiJ1c2VyQGdtYWlsLmNvbSIsImlhdCI6MTcwMDM1MTY4NSwiZXhwIjoxNzAwMzU1Mjg1fQ.go1bLf_qXOYJSHNe0-IBkM6m1vCcrW6IHOIqBCeWKRs`,
+		},
+	});
 
 	if (error) return <div>Error: {error.message}</div>;
 	if (!data) return <div>Loading...</div>;
 
-	console.log(data)
+	console.log(data);
 	return (
 		<div>
-		
+			<h1>{data.data.email}</h1>
 			<div>
 				<Link to="/me/portfolios/create">Create new portfolio</Link>
 			</div>
