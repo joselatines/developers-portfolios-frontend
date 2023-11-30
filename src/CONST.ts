fc2efc8f63bfe238e19d0aa2c4db0cefc61b4c25
Jose Latines <joselatines33@gmail.com>
2023-11-30 18:54:02 -0400

    fix: enviroment

--- src/CONST.ts
+++ src/CONST.ts
@@ -1,4 +1,6 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const DEFAULT_PROFILE_PIC =
-	"https://i.pinimg.com/236x/5e/1e/b8/5e1eb8c109741e5559814f940428bcb8.jpg";
+	"https://static.vecteezy.com/system/resources/thumbnails/030/504/836/small/avatar-account-flat-isolated-on-transparent-background-for-graphic-and-web-design-default-social-media-profile-photo-symbol-profile-and-people-silhouette-user-icon-vector.jpg";
+export const DEFAULT_PORTFOLIO_PIC =
+	"/public/image-placeholder.webp";
 export const USER_KEY_LOCAL_STORAGE = "user";
